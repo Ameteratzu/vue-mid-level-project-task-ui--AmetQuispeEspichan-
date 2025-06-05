9fb60d9d7b44fa85c0388e0a8ff67e9c53f076b2
Amet Marcelo <125571134+Ameteratzu@users.noreply.github.com>
2025-06-05 13:20:08 -0500

    mejoras totales visuales

--- .history/src/components/ProjectForm_20250605122253.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="p-4 bg-white rounded shadow">
-
-    </div>
-</template>
-
-<script setup>
-</script>
--- .history/src/views/Loader_20250605123702.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="fixed inset-0 flex items-center justify-center bg-white/50">
-        <div class="loader animate-spin rounded-full h-12 w-12 border-4 border-blue-500"></div>
-    </div>
-</template>
-
-<style>
-    .loader { border-top-color: transparent;}
-    </style>
